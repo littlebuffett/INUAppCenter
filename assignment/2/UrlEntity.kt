58a5aaf86ae139ccd94a4180326d7ea95c753391
강병하 <76998855+littlebuffett@users.noreply.github.com>
2021-12-16 23:25:55 +0900

    Update UrlEntity.kt

--- assignment/2/UrlEntity.kt
+++ assignment/2/UrlEntity.kt
@@ -1,10 +1,10 @@
 package com.example.appcentertask2
 
-class UrlEntity { //전체 -> result -> url
-    var result: Result? = null
+class UrlEntity { //JSON 구조  => 전체 -> result -> url
+    var result: Result? = null //result에는 "result"배열에대응된다. (이름이 같아서? 구조가 같아서?)
 
     inner class Result {
-        var url: String? = null
+        var url: String? = null //"result" 배열 안에 있는 "url"의 값에 대응된다. ("url"은 key)
     }
 
 }
