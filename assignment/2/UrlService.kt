ef09c52a05de3a31ac0c8a5f2f44c3ccbe4383e1
강병하 <76998855+littlebuffett@users.noreply.github.com>
2021-12-18 11:30:18 +0900

    Update UrlService.kt

--- assignment/2/UrlService.kt
+++ assignment/2/UrlService.kt
@@ -8,5 +8,5 @@ interface UrlService {
     @POST("shorturl")
     fun requestTranslation (@Header("X-Naver-Client-Id")clientID: String = Configs.clientID,
                             @Header("X-Naver-Client-Secret")apiKey: String = Configs.apiKey,
-                            @Field("url")originalUrl: String = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EA%B0%9C%EB%B0%9C%EC%9E%90%EC%84%BC%ED%84%B0") : Call<UrlEntity>
+                            @Field("url")originalUrl: String = inputUrl ) : Call<UrlEntity>
 }
